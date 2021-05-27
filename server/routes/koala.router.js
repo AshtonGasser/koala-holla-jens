d9fb2a0776dd4adc73482472e51d602adc2f658b
Alex Kim <alexkim251@yahoo.com>
2021-05-27 14:35:51 -0500

    added router.post syntax for koala properties

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -6,9 +6,22 @@ const koalaRouter = express.Router();
 
 // GET
 
-
 // POST
-
+router.post('/',  (req, res) => {
+    let newKoala = req.body;
+    console.log(`Adding Koala`, newKoala);
+  
+    let queryText = `INSERT INTO "koalas" ("name", "gender", "age", "ready-to-transfer", "notes")
+                     VALUES ($1, $2, $3, $4, $5);`;
+    pool.query(queryText, [newKoala.name, nnewKoala.gender, newKoala.age, newKoala.readyForTranfer, newKoala.notes])
+      .then(result => {
+        res.sendStatus(201);
+      })
+      .catch(error => {
+        console.log(`Error adding new koala`, error);
+        res.sendStatus(500);
+      });
+  });
 
 // PUT
 
