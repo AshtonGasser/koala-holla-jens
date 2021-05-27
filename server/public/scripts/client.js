96dce767b7d2429e0f4f9f9806e8604ac714ca9a
Emry Brisky <eabrisky@gmail.com>
2021-05-27 14:50:26 -0500

    setup get route and pool

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -30,7 +30,17 @@ function setupClickListeners() {
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
-  
+  $.ajax({
+    method: 'GET',
+    url: '/koalas'
+  })
+  .then(function (response) {
+    console.log(response);
+    saveKoala(response);
+  })
+  .catch(error =>{
+    console.log('error in GET of koalas', error);
+  })
 } // end getKoalas
 
 function saveKoala( newKoala ){
