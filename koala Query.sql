fec6d4d7e147f6616723bd963d689b0f854345f9
Ashton Gasser <ashtongasser@gmail.com>
2021-05-28 09:43:47 -0500

    button fix

---
+++ koala Query.sql
@@ -0,0 +1,26 @@
+CREATE TABLE "koalas" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(255) NOT NULL,
+	"gender" VARCHAR (255) NOT NULL,
+	"age" INT,
+	"ready-for-transfer" BOOLEAN DEFAULT FALSE,
+	"notes" VARCHAR(255)
+);
+
+INSERT INTO "koalas" ("id", "name", "gender", "age", "ready-to-transfer", "notes")
+VALUES (1, 'Scotty', 'M', 4, TRUE, 'Born in Guatemala');
+
+INSERT INTO "koalas" ("id", "name", "gender", "age", "ready-to-transfer", "notes")
+VALUES (2, 'Jean', 'F', 5, TRUE, 'Allergic to lots of lava');
+
+INSERT INTO "koalas" ("id", "name", "gender", "age", "ready-to-transfer", "notes")
+VALUES (3, 'Ororo', 'F', 7, FALSE, 'Loves listening to Paula Abdul');
+
+INSERT INTO "koalas" ("id", "name", "gender", "age", "ready-to-transfer", "notes")
+VALUES (4, 'Logan', 'M', 15, FALSE, 'Loves the sauna');
+
+INSERT INTO "koalas" ("id", "name", "gender", "age", "ready-to-transfer", "notes")
+VALUES (5, 'Charlie', 'M', 9, TRUE, 'Favorite band is Nirvana');
+
+INSERT INTO "koalas" ("id", "name", "gender", "age", "ready-to-transfer", "notes")
+VALUES (6, 'Betsy', 'F', 4, TRUE, 'Has a pet iguana');
